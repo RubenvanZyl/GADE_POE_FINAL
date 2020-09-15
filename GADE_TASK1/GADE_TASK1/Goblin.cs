4d4d2e9bada49ab8612d8a341aa5e7f21b7f4ecb
Ruben Alexander Van Zyl <20104142@vegaconnect.co.za>
2020-09-15 15:16:11 +0200

    Added last class

--- GADE_TASK1/GADE_TASK1/Goblin.cs
+++ GADE_TASK1/GADE_TASK1/Goblin.cs
@@ -8,5 +8,12 @@ namespace GADE_TASK1
 {
     class Goblin
     {
+        public Goblin(int Xinput, int Yinput, int HP)
+        {
+            this.HP - HP;
+            maxHP - HP;
+            damage = 1;
+        }
+
     }
 }
